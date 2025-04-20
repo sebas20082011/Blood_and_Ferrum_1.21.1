395c70d5c58a8bd7617e0d670c6ed08077f731d2
sebas2008 <sebastianbergallo01@gmail.com>
2025-04-19 20:04:07 -0400

    skibidi

--- src/main/java/com/sebas2008/blood_and_ferrum/Blood_and_Ferrum.java
+++ src/main/java/com/sebas2008/blood_and_ferrum/Blood_and_Ferrum.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-// I love fortnite
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Blood_and_Ferrum.MOD_ID)
 public class Blood_and_Ferrum {
