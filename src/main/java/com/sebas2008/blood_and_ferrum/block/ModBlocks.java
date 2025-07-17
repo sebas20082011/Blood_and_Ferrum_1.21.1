42e74c1bac5b389dba2bc2b87012215c9cf10dc2
sebas2008 <sebastianbergallo01@gmail.com>
2025-07-16 22:57:38 -0400

    new things

--- src/main/java/com/sebas2008/blood_and_ferrum/block/ModBlocks.java
+++ src/main/java/com/sebas2008/blood_and_ferrum/block/ModBlocks.java
@@ -3,9 +3,11 @@ package com.sebas2008.blood_and_ferrum.block;
 
 import com.sebas2008.blood_and_ferrum.Blood_and_Ferrum;
 import com.sebas2008.blood_and_ferrum.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,20 +23,20 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, Blood_and_Ferrum.MOD_ID);
 
     public static final RegistryObject<Block> ZINC_ORE  =   registerBlock("zincore_block",
-            () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).destroyTime(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+            () -> new DropExperienceBlock(UniformInt.of(2,4 ),BlockBehaviour.Properties.of()
+                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     public static final RegistryObject<Block> LEAD_ORE  =   registerBlock("leadore_block",
-            () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).destroyTime(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+            () -> new DropExperienceBlock(UniformInt.of(2,4 ),BlockBehaviour.Properties.of()
+                    .strength(2f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     public static final RegistryObject<Block> STEEL_BLOCK  =   registerBlock("steel_block",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).destroyTime(4f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(4).requiresCorrectToolForDrops().sound(SoundType.METAL)));
 
     public static final RegistryObject<Block> BRASS_BLOCK  =   registerBlock("brass_block",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).destroyTime(4f).requiresCorrectToolForDrops().sound(SoundType.COPPER)));
+                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.COPPER)));
 
 private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
     RegistryObject<T> toReturn = BLOCKS.register(name, block);
